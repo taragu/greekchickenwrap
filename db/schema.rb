05f5d1e29f5850d69fb153460cada38b040e0043
taragu <tara.weiqing@gmail.com>
2014-11-16 00:52:02 -0800

    added more questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116064215) do
+ActiveRecord::Schema.define(version: 20141116085012) do
 
   create_table "users", force: true do |t|
-    t.string   "email",                     default: "", null: false
-    t.string   "encrypted_password",        default: "", null: false
+    t.string   "email",                        default: "", null: false
+    t.string   "encrypted_password",           default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",             default: 0,  null: false
+    t.integer  "sign_in_count",                default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20141116064215) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.integer  "administering_medications", default: 0
+    t.integer  "administering_medications",    default: 0
+    t.integer  "observing_patients_over_time", default: 0
+    t.integer  "changing_dressings",           default: 0
+    t.integer  "taking_and_recording_vitals",  default: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
