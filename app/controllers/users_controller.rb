f31f980b834207f231bdbe1fa4448546ba0b06bb
taragu <tara.weiqing@gmail.com>
2014-11-15 23:59:27 -0800

    able to add one question; i think it works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,33 @@ class UsersController < ApplicationController
       redirect_to :back, :alert => "Access denied."
     end
   end
+  
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def edit2
+    @user = User.find(params[:id])
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render action: 'edit'
+    end
+  end
+
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :created_at, :updated_at, :name, :administering_medications);
+  end
 
 end
